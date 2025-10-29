56d1b5e24555dd72795b5cf855bb603e436c26ab
Daniel Myers <64658614+Danluk1001@users.noreply.github.com>
2025-10-29 17:30:30 -0400

    Add files via upload

---
+++ HideAndSeek.java
@@ -0,0 +1,10 @@
+public class HideAndSeek {
+
+    public static void main(String[] args) {
+        System.out.println("Let's Play Hide And Seek");
+        System.out.print("Three...");
+        System.out.print("Two...");
+        System.out.println("One...");
+        System.out.println("Ready Or Not, Here I Come!");
+    }
+}
